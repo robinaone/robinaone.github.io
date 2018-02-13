8c061ce88974e58f0f12ae67008eb2a3538d8d72
Caleb Hargraves <jmjoyh00@gmail.com>
2018-02-13 13:19:10 -0700

    Dynamic Textboxes

---
+++ JavaScript/dynamic.js
@@ -0,0 +1,35 @@
+function createBox() {
+  textBox = document.createElement('input');
+  textBox.setAttribute('value', '1');
+  textBox.setAttribute('onkeyup', 'validateTextbox(this)');
+  textBox.className = 'numeric';
+  document.getElementById('textBoxes').appendChild(textBox);
+}
+
+function add() {
+  boxes = Array.from(document.getElementsByClassName('numeric'));
+  sum = 0;
+  boxes.forEach(function(elem) {
+    sum += parseInt(elem.value);
+  });
+  document.getElementById('answer').innerHTML = sum;
+}
+
+function validateTextbox(textBox) {
+  num = textBox.value;
+  if (isNaN(num)) {
+    textBox.className = 'numeric invalid';
+  } else {
+    textBox.className = 'numeric';
+  }
+  setButtonState();
+}
+
+function setButtonState() {
+  invalids = document.getElementsByClassName('invalid');
+  if (invalids.length > 0) {
+      document.getElementById('addButton').disabled = true;
+  } else {
+    document.getElementById('addButton').disabled = false;
+  }
+}
