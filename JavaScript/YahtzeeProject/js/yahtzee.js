a939d57ff0a4846a3917742c82591aaed409502f
Caleb Hargraves <jmjoyh00@gmail.com>
2018-03-11 21:46:32 -0500

    Finished (ish) Yahtzee

--- JavaScript/YahtzeeProject/js/yahtzee.js
+++ JavaScript/YahtzeeProject/js/yahtzee.js
@@ -97,11 +97,276 @@ function conditionIsMet (condition) {
 }
 
 function ofAKind(condition) {
-	return false;
+  numberOfDuplicates = cacheArray();
+  arrayForFour = buildSides();
+  fourIsTrue = fourEval(arrayForFour);
+  threeIsTrue = threeEval(arrayForFour);
+  yahtzeeIsTrue = yahtzeeEval(arrayForFour);
+  if (condition[1] == 3 && threeIsTrue == true) {
+    return true;
+  }
+  if (condition[1] == 4 && fourIsTrue == true) {
+    return true;
+  }
+  if (condition[1] == 2 && numberOfDuplicates >= 4) {
+    return true;
+  }
+  if (condition[1] == 5 && yahtzeeIsTrue == true) {
+    return true;
+  }
 }
 
 function inARow(condition) {
-	return false;
+  arrayForRow = buildSides();
+  smIsTrue = smEval(arrayForRow);
+  lgIsTrue = lgEval(arrayForRow);
+  if (condition[1] == 4 && smIsTrue == true) {
+    return true;
+  }
+  if (condition[1] == 5 && lgIsTrue == true) {
+    return true;
+  }
+}
+
+function buildSides() {
+  cache = [];
+  yahtzee.dice.forEach(function(die) {
+    cache.push(die.sideUp);
+  })
+  cache = cache.sort();
+  return cache;
+}
+
+function cacheArray() {
+  cache = [];
+  yahtzee.dice.forEach(function(die) {
+    cache.push(die.sideUp);
+  })
+  cache = cache.sort();
+
+  var numberOfDuplicates = [];
+  for (var i = 0; i < cache.length - 1; i++) {
+    if (cache[i + 1] == cache[i]) {
+      numberOfDuplicates.push(cache[i]);
+    }
+  }
+  if (numberOfDuplicates.length == 3) {
+    var fullHouse = [];
+    for (i = 0; i < numberOfDuplicates.length - 1; i++) {
+      if (numberOfDuplicates[i + 1] != numberOfDuplicates[i]) {
+        fullHouse.push(cache[i]);
+      }
+    }
+    if (fullHouse.length >= 1) {
+      return 7355608;
+    }
+  } else {
+    return numberOfDuplicates.length;
+  }
+}
+
+function lgEval(arrayMod) {
+  arrayMod = arrayMod.sort();
+  var numberInARow = [];
+  for (var i = 0; i < arrayMod.length; i++) {
+    if (arrayMod[i] + 1 == arrayMod[i + 1]) {
+      numberInARow.push(arrayMod[i]);
+    }
+    if (i == arrayMod.length - 1) {
+      if (arrayMod[i] - 1 == arrayMod[i - 1]) {
+        numberInARow.push(arrayMod[i]);
+      }
+    }
+  }
+
+  if (numberInARow.length == 5) {
+    return true;
+  }
+}
+
+function fourEval(arrayMod) {
+  const ones = arrayMod.filter(function(it) {
+    return it === 1;
+  });
+  const twos = arrayMod.filter(function(it) {
+    return it === 2;
+  });
+  const threes = arrayMod.filter(function(it) {
+    return it === 3;
+  });
+  const fours = arrayMod.filter(function(it) {
+    return it === 4;
+  });
+  const fives = arrayMod.filter(function(it) {
+    return it === 5;
+  });
+  const sixes = arrayMod.filter(function(it) {
+    return it === 6;
+  });
+  if (ones.length == 4) {
+    return true;
+  }
+  if (twos.length == 4) {
+    return true;
+  }
+  if (threes.length == 4) {
+    return true;
+  }
+  if (fours.length == 4) {
+    return true;
+  }
+  if (fives.length == 4) {
+    return true;
+  }
+  if (sixes.length == 4) {
+    return true;
+  }
+}
+
+function yahtzeeEval(arrayMod) {
+  const ones = arrayMod.filter(function(it) {
+    return it === 1;
+  });
+  const twos = arrayMod.filter(function(it) {
+    return it === 2;
+  });
+  const threes = arrayMod.filter(function(it) {
+    return it === 3;
+  });
+  const fours = arrayMod.filter(function(it) {
+    return it === 4;
+  });
+  const fives = arrayMod.filter(function(it) {
+    return it === 5;
+  });
+  const sixes = arrayMod.filter(function(it) {
+    return it === 6;
+  });
+  if (ones.length == 5) {
+    return true;
+  }
+  if (twos.length == 5) {
+    return true;
+  }
+  if (threes.length == 5) {
+    return true;
+  }
+  if (fours.length == 5) {
+    return true;
+  }
+  if (fives.length == 5) {
+    return true;
+  }
+  if (sixes.length == 5) {
+    return true;
+  }
+}
+
+function threeEval(arrayMod) {
+  const ones = arrayMod.filter(function(it) {
+    return it === 1;
+  });
+  const twos = arrayMod.filter(function(it) {
+    return it === 2;
+  });
+  const threes = arrayMod.filter(function(it) {
+    return it === 3;
+  });
+  const fours = arrayMod.filter(function(it) {
+    return it === 4;
+  });
+  const fives = arrayMod.filter(function(it) {
+    return it === 5;
+  });
+  const sixes = arrayMod.filter(function(it) {
+    return it === 6;
+  });
+  if (ones.length == 3) {
+    return true;
+  }
+  if (twos.length == 3) {
+    return true;
+  }
+  if (threes.length == 3) {
+    return true;
+  }
+  if (fours.length == 3) {
+    return true;
+  }
+  if (fives.length == 3) {
+    return true;
+  }
+  if (sixes.length == 3) {
+    return true;
+  }
+}
+
+function smEval(arrayMod) {
+  arrayMod = arrayMod.sort();
+  duplicateStrip = [];
+  for (i = 0; i < arrayMod.length; i++) {
+    if (duplicateStrip.indexOf(arrayMod[i]) == -1) {
+      duplicateStrip.push(arrayMod[i])
+    }
+  }
+  arrayMod = duplicateStrip;
+
+  var numberInARow = [];
+  for (var i = 0; i < arrayMod.length; i++) {
+    if (arrayMod[i] + 1 == arrayMod[i + 1] || arrayMod[i] - 1 == arrayMod[i - 1]) {
+      numberInARow.push(arrayMod[i]);
+    }
+    if (i == 2) {
+      if (arrayMod[i] + 1 != arrayMod[i + 1] || arrayMod[i] - 1 != arrayMod[i - 1]) {
+        return false;
+      }
+    }
+  }
+  duplicateStrip = [];
+  for (i = 0; i < numberInARow.length; i++) {
+    if (duplicateStrip.indexOf(numberInARow[i]) == -1) {
+      duplicateStrip.push(numberInARow[i])
+    }
+  }
+  if (duplicateStrip.length >= 4) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+function sumOfDice(valueToMatch) {
+  total = 0;
+  yahtzee.dice.forEach(function(die) {
+    if (die.sideUp == valueToMatch || valueToMatch === 0) {
+      total += die.sideUp;
+    }
+  })
+  return total;
+}
+
+function topSubCalc() {
+  subAccum = 0;
+  for (i = 0; i < 6; i++) {
+    subAccum += yahtzee.scoreCard[i].score;
+  }
+  return subAccum;
+}
+
+function topBonusCalc(topScore) {
+  if (topScore >= 63) {
+    return 35;
+  } else {
+    return 0;
+  }
+}
+
+function totalScoreCalc(bonus) {
+  scoreAccum = 0;
+  for (i = 0; i < yahtzee.scoreCard.length; i++) {
+    scoreAccum += yahtzee.scoreCard[i].score;
+  }
+  return scoreAccum + bonus;
 }
 
 function loadScoreCard() {
@@ -116,14 +381,14 @@ function loadScoreCard() {
       buildScoreCardRow(scoreCardRow.title, scoreCardRow.score, (scoreCardRow.scoreRecorded ? "scored" : "unscored"), !scoreCardRow.scoreRecorded, index);
     }
   });
-  buildScoreCardRow("Top Subtotal:", " ", "totals", false, 0);
-  buildScoreCardRow("Top Bonus:", " ", "totals", false, 0);
+  buildScoreCardRow("Top Subtotal:", " ", "totals topSubtotal", false, 0);
+  buildScoreCardRow("Top Bonus:", " ", "totals topBonus", false, 0);
   yahtzee.scoreCard.forEach(function(scoreCardRow, index) {
     if (!scoreCardRow.top) {
       buildScoreCardRow(scoreCardRow.title, scoreCardRow.score, (scoreCardRow.scoreRecorded ? "scored" : "unscored"), !scoreCardRow.scoreRecorded, index);
     }
   });
-  buildScoreCardRow("Total Score:", " ", "totals", false, 0);
+  buildScoreCardRow("Total Score:", " ", "totals totalScore", false, 0);
 }
 
 function buildScoreCardRow(title, score, columnClassName, clickable, scorecardIndex) {
@@ -170,9 +435,11 @@ function saveScore() {
     die.sideUp = 0;
     die.saved = false;
   });
-  if (scoreSaved)
     yahtzee.turnsRemaining--;
   loadDice();
+  if (yahtzee.turnsRemaining <= 0) {
+    endGame();
+  }
   }
 }
 
@@ -181,6 +448,20 @@ function saveDie(dieIndex) {
   yahtzee.dice[dieIndex].saved = !yahtzee.dice[dieIndex].saved;
   }
     loadDice();
+}
+
+function endGame() {
+  topScore = topSubCalc();
+  topSub = document.getElementsByClassName("topSubtotal");
+  topSub[0].innerHTML = topScore;
+
+  bonusScore = topBonusCalc(topScore);
+  topBonus = document.getElementsByClassName("topBonus");
+  topBonus[0].innerHTML = bonusScore;
+
+  finalScore = totalScoreCalc(bonusScore);
+  totalScore = document.getElementsByClassName("totalScore");
+  totalScore[0].innerHTML = finalScore;
 }
 
 function newGame() {
